05645d6cb1b7f32da4c8c03283fb410f073d8fa4
Max05643 <05643mak@gmail.com>
2024-02-23 15:40:15 +0100

    Added moves validator

---
+++ ChessDefinitions/IMoveValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessDefinitions
+{
+    /// <summary>
+    /// Provdies a way to validate moves inputted by players
+    /// </summary>
+    public interface IMoveValidator
+    {
+        bool Validate(string move);
+    }
+}
---
+++ ChessGameRepresentation/MoveValidator.cs
@@ -0,0 +1,22 @@
+﻿using ChessDefinitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ChessGameRepresentation
+{
+    /// <summary>
+    /// Validates move accordingly to a format that ChessGameState consumes
+    /// </summary>
+    public class MoveValidator : IMoveValidator
+    {
+        private readonly Regex moveRegex = new Regex(@"^[a-h][1-8][a-h][1-8]$");
+        bool IMoveValidator.Validate(string move)
+        {
+            return moveRegex.IsMatch(move);
+        }
+    }
+}
