05f2624a2258bdec5f5eb1d451136789daa1527e
Max05643 <05643mak@gmail.com>
2024-02-27 19:31:50 +0100

    Added logging into ChessGameController

--- ChessBotDiscord/Program.cs
+++ ChessBotDiscord/Program.cs
@@ -4,17 +4,21 @@ using ChessDefinitions;
 using ChessGameRepresentation;
 using ChessBotDiscord;
 using ChessGameControllerImplementation;
+using Microsoft.Extensions.Logging;
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .AddEnvironmentVariables()
     .Build();
 
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+
 IChessAI stockFish = new StockfishWrapper.Stockfish(configuration["PathToStockFish"]!, 100);
 IChessGameFactory gameFactory = new ChessGameStateFactory();
 IMoveValidator moveValidator = new MoveValidator();
 IGamesStorage gamesStorage = new InMemoryGamesStorage();
-IChessGamesController chessGamesController = new ChessGameController(stockFish, gameFactory, moveValidator, gamesStorage);
+
+IChessGamesController chessGamesController = new ChessGameController(stockFish, gameFactory, moveValidator, gamesStorage, factory.CreateLogger<ChessGameController>());
 IBoardVisualizer boardVisualizer = new BoardVisualizer.WebBoardVisualizer();
 ILocalizationProvider localizationProvider = new ConfigLocalizationProvider(configuration);
 
--- ChessGameControllerImplementation/ChessGameController.cs
+++ ChessGameControllerImplementation/ChessGameController.cs
@@ -1,4 +1,5 @@
 ﻿using ChessDefinitions;
+using Microsoft.Extensions.Logging;
 using System.Collections.Concurrent;
 
 namespace ChessGameControllerImplementation;
@@ -13,67 +14,83 @@ public class ChessGameController : IChessGamesController
     readonly IChessGameFactory gameFactory;
     readonly IMoveValidator moveValidator;
     readonly IGamesStorage gamesStorage;
+    readonly ILogger<ChessGameController> logger;
 
-    public ChessGameController(IChessAI chessAI, IChessGameFactory gameFactory, IMoveValidator moveValidator, IGamesStorage gamesStorage)
+    public ChessGameController(IChessAI chessAI, IChessGameFactory gameFactory, IMoveValidator moveValidator, IGamesStorage gamesStorage, ILogger<ChessGameController> logger)
     {
         this.chessAI = chessAI;
         this.gameFactory = gameFactory;
         this.moveValidator = moveValidator;
         this.gamesStorage = gamesStorage;
+        this.logger = logger;
     }
 
     IChessGamesController.MoveRequestResult IChessGamesController.MakeMove(string gameId, string move, out IChessGame? currentGameState)
     {
         currentGameState = null;
-        using var gameHandler = gamesStorage.AcquireGame(gameId);
 
-        if (gameHandler != null)
+
+        try
         {
-            currentGameState = gameHandler.Game.Clone();
-
-            if (!moveValidator.Validate(move))
+            using var gameHandler = gamesStorage.AcquireGame(gameId);
+            if (gameHandler != null)
             {
-                return IChessGamesController.MoveRequestResult.WrongFormat;
-            }
-
-            if (gameHandler.Game.GetCurrentState() != IChessGame.GameState.InProgress)
-            {
-                return IChessGamesController.MoveRequestResult.GameAlreadyEnded;
-            }
-
-            if (gameHandler.Game.MakeMove(move))
-            {
-                chessAI.GetNextMove(gameHandler.Game.GetFen(), out string? aiMove);
-                gameHandler.Game.MakeMove(aiMove!);
-
-
                 currentGameState = gameHandler.Game.Clone();
-                return IChessGamesController.MoveRequestResult.Success;
+
+                if (!moveValidator.Validate(move))
+                {
+                    return IChessGamesController.MoveRequestResult.WrongFormat;
+                }
+
+                if (gameHandler.Game.GetCurrentState() != IChessGame.GameState.InProgress)
+                {
+                    return IChessGamesController.MoveRequestResult.GameAlreadyEnded;
+                }
+
+                if (gameHandler.Game.MakeMove(move))
+                {
+                    chessAI.GetNextMove(gameHandler.Game.GetFen(), out string? aiMove);
+                    gameHandler.Game.MakeMove(aiMove!);
+
+
+                    currentGameState = gameHandler.Game.Clone();
+                    return IChessGamesController.MoveRequestResult.Success;
+                }
+                else
+                {
+                    return IChessGamesController.MoveRequestResult.IllegalMove;
+                }
+
             }
             else
             {
-                return IChessGamesController.MoveRequestResult.IllegalMove;
+                return IChessGamesController.MoveRequestResult.GameNotFound;
             }
-
         }
-        else
+        catch (Exception e)
         {
-            return IChessGamesController.MoveRequestResult.GameNotFound;
+            logger.LogError("Error in MakeMove: {e}", e);
+            return IChessGamesController.MoveRequestResult.InternalError;
         }
     }
 
     IChessGame IChessGamesController.StartNewGame(string gameId, bool isPlayerWhite)
     {
         var newGameState = gameFactory.CreateGame(isPlayerWhite);
-
-        if (!isPlayerWhite)
+        try
         {
-            chessAI.GetNextMove(newGameState.GetFen(), out string? aiMove);
-            newGameState.MakeMove(aiMove!);
+            if (!isPlayerWhite)
+            {
+                chessAI.GetNextMove(newGameState.GetFen(), out string? aiMove);
+                newGameState.MakeMove(aiMove!);
+            }
+
+            gamesStorage.CreateGame(gameId, newGameState);
         }
-
-        gamesStorage.CreateGame(gameId, newGameState);
-
+        catch (Exception e)
+        {
+            logger.LogError("Error in StartNewGame: {e}", e);
+        }
 
         return newGameState.Clone();
     }
